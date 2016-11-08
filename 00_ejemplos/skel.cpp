ffe4e4a1d0e477223363bfa0b5cac4fdc36e11b5
diego <diegop.m.lasnaves@gmail.com>
2016-11-08 18:27:24 +0100

    añadiendo nuevo archivo a github.

---
+++ 00_ejemplos/skel.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>  */Para trabajar con tuberias*/
+#include <stdlib.h>
+
+int main(){
+
+	return EXIT_SUCCESS;
+
+}
+
+
+*/Este código lo que hace es cargar las librerias que hemos puesto con #include, y despues hemos definido un "main" que llama a una función. Despues el Return lo que hace es que vuelva y "EXIT_SUCCESS" lo que hace es referencia al nº 0*/
+
+
+
