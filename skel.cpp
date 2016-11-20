750c59927228a382f67a79a149bafe6bd415ba9d
diego <diegop.m.lasnaves@gmail.com>
2016-11-20 20:07:17 +0100

    .

--- skel.cpp
+++ skel.cpp
@@ -1,14 +1,13 @@
-#include <stdio.h>  */Para trabajar con tuberias*/
+#include <stdio.h>
 #include <stdlib.h>
 
 int main(){
 
-	return EXIT_SUCCESS;
 
+
+ return EXIT_SUCCESS;
 }
 
-
-*/Este código lo que hace es cargar las librerias que hemos puesto con #include, y despues hemos definido un "main" que llama a una función. Despues el Return lo que hace es que vuelva y "EXIT_SUCCESS" lo que hace es referencia al nº 0*/
 
 
 
